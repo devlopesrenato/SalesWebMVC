5bf7fc494bc476f0133227bf7be8bee2f2d8c84b
projetosRenatoLopes <tirenatolopes@gmail.com>
2023-12-19 11:29:45 -0300

    Others entities implementation

---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,27 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amounnt { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Saller { get; set; }
+
+        public SalesRecord() 
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amounnt, SalesStatus status, Seller saller)
+        {
+            Id = id;
+            Date = date;
+            Amounnt = amounnt;
+            Status = status;
+            Saller = saller;
+        }
+    }
+}
