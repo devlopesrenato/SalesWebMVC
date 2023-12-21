53563488c9660f7528774e8ab474371e87490f8e
devLopesRenato <renatolopes37@gmail.com>
2023-12-21 00:44:28 -0300

    feat: Configuring validations for sales record

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -1,13 +1,21 @@
 ﻿using SalesWebMVC.Models.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMVC.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.DateTime)]
         public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
+
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
 
